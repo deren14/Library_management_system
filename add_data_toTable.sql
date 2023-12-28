956f49475fed98a57145b9427500d63cb91b3388
Dilek Eren <108549514+deren14@users.noreply.github.com>
2023-12-28 18:45:50 +0300

    Add files via upload

---
+++ add_data_toTable.sql
@@ -0,0 +1,7 @@
+insert into members
+values
+("Dilek Eren","dilekeren13@gmail.com","2020-12-28"),
+("Koray Sakin","koraysakin@gmail.com","2021-05-12"),
+("Özge Günaydın","özgegün12@gmail.com","2022-1-08");
+
+select * from members;
